b0d5151a7ca6a38f53927376f490c00773d663a1
bensuz <elifbensuaslan@gmail.com>
2023-10-11 18:08:25 +0100

    index changed

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     })
 );
